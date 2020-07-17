c006153dc32dbb99804ccfae66ebd19602be2618
王飞 <2494662681@qq.com>
2020-07-17 21:35:22 +0800

    rwlock for redis

--- rwlock.go
+++ rwlock.go
@@ -1,31 +1,37 @@
 package rwlock
 
+import (
+	"github.com/wangfeiso/rwlock/client"
+	"github.com/wangfeiso/rwlock/tool"
+)
+
 type RWLock struct {
 	shaHashID *string
 	lockKey   string
 	uniqID    string
+	expire    int64
 }
 
 func New(key string) *RWLock {
 	return &RWLock{
-		shaHashID: shaHashID,
-		lockKey:   key,
-		uniqID:    "random_str_",
+		lockKey: key,
+		uniqID:  tool.GetUUID(),
+		expire:  10,
 	}
 }
 
 func (l *RWLock) Lock() {
-
+	client.Lock(l.lockKey, l.uniqID, l.expire)
 }
 
 func (l *RWLock) Unlock() {
-
+	client.Unlock(l.lockKey, l.uniqID)
 }
 
 func (l *RWLock) RLock() {
-
+	client.RLock(l.lockKey)
 }
 
 func (l *RWLock) RUnlock() {
-
+	client.RLock(l.lockKey)
 }
